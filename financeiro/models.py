fa76d068320949ac29ca3ab0f0e25cd4ef78f3a9
Kawan <kawanhenrike6k@gmail.com>
2026-02-20 18:51:18 -0300

    criado models de gasto e entrada

--- financeiro/models.py
+++ financeiro/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+
+class Entradas(models.Model):
+    data = models.DateField()
+    origem = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10)
+
+    def __str__(self):
+        return f"{self.origem} - {self.valor}"
+
+class Gasto(models.Model):
+    tipo_escolha = [
+        ('dinheiro', 'Dinheiro'),
+        ('pix', 'Pix'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    registro = models.DateTimeField()
+    tipo = models.CharField(choices=tipo_escolha, default='dinheiro')
+    categoria = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10)
+
+    def __str__(self):
+        return f"O gasto é {self.nome}"
