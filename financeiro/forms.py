bf58392d1f7d0f9568bcb0b181889010238baf89
Kawan <kawanhenrike6k@gmail.com>
2026-02-21 19:41:17 -0300

    criado os 2 formularios no forms

--- financeiro/forms.py
+++ financeiro/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Gastos, Entradas, Fixos, Cartao
+from .models import Gastos, Entradas, Fixos, Cartao, Caixinhas, Dividas
 
 class EntradasForm(forms.ModelForm):
     class Meta:
@@ -45,4 +45,26 @@ class CartaoForm(forms.ModelForm):
             'parcela': forms.NumberInput(attrs={'class': 'input_parcela'}),
             'categoria': forms.TextInput(attrs={'class': 'input_categoria'}),
             'valor': forms.NumberInput(attrs={'class': 'input_valor'}),
+        }
+
+class CaixinhasForm(forms.ModelForm):
+    class Meta:
+        model = Caixinhas
+        fields = ['objetivo', 'meta', 'guardado', 'falta']
+        widgets = {
+            'objetivo': forms.TextInput(attrs={'class': 'input_objetivo'}),
+            'meta': forms.NumberInput(attrs={'class': 'input_meta'}),
+            'guardado': forms.NumberInput(attrs={'class': 'input_guardado'}),
+            'falta': forms.NumberInput(attrs={'class': 'input_falta'})
+        }
+
+class DividasForm(forms.ModelForm):
+    class Meta:
+        model = Dividas
+        fields = ['nome', 'valor_parcela', 'total_parcela', 'data']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'input_objetivo'}),
+            'valor_parcela': forms.NumberInput(attrs={'class': 'input_valor_parcela'}),
+            'total_parcela': forms.NumberInput(attrs={'class': 'total_parcela'}),
+            'data': forms.DateInput(attrs={'class': 'input_data'})
         }
