f305aacb415ceec15a457bd39cd61cec9422c420
Kawan <kawanhenrike6k@gmail.com>
2026-02-21 00:35:56 -0300

    horas depois, front de cadastrar gastos pronta

--- financeiro/forms.py
+++ financeiro/forms.py
@@ -14,5 +14,9 @@ class GastosForm(forms.ModelForm):
         model = Gastos
         fields = ['nome', 'registro', 'tipo', 'categoria', 'valor']
         widgets = {
-            'registro': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+            'nome': forms.TextInput(attrs={'class': 'input_nome'}),
+            'registro': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'input_tipo'}),
+            'tipo': forms.Select(attrs={'class': 'input_tipo'}),
+            'categoria': forms.TextInput(attrs={'class': 'input_categoria'}),
+            'valor': forms.NumberInput(attrs={'class': 'input_valor'})
         }
