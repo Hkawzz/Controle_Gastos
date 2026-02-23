9bcd11894742629cb6b31525aebc50d2d3b401bb
Kawan <kawanhenrike6k@gmail.com>
2026-02-23 01:50:15 -0300

    saldo visivel na visao geral

--- financeiro/views.py
+++ financeiro/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from gastos.models import Gastos
+from entradas.models import Entradas
+from fixos.models import Fixos
+from cartao.models import Cartao
+from caixinhas.models import Caixinhas
+from dividas.models import Dividas
 
 def visao_geral(request):
-    return render(request, "visao_geral/index.html")
+    saldo_total = saldo()
+    
+    return render(request, "visao_geral/index.html", {'saldo_total': saldo_total})
+
+def saldo():
+    gastos = Gastos.objects.all()
+    entradas = Entradas.objects.all()
+
+    total_gastos = sum(item.valor for item in gastos)
+    total_entradas = sum(item.valor for item in entradas)
+    saldo = total_entradas - total_gastos
+
+    return saldo
